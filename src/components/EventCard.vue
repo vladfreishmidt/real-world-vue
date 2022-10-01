031b979a68e21870ba8fdcbf0f71537630cc74be
Vlad <vladfreishmidt.webdevelopment@gmail.com>
2022-10-01 23:46:04 +0300

    clean up

---
+++ src/components/EventCard.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="event-card"></div>
+</template>
+
+<script>
+export default {
+    name: 'EventCard',
+    props: {
+        msg: String
+    }
+};
+</script>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div>
+        <EventCard />
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import EventCard from '@/components/EventCard.vue';
 
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
+    name: 'HomeView',
+    components: {
+        EventCard
+    }
 };
 </script>
